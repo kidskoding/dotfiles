659d29c534b2f6db70b050cbcd7134aee56f5fe6
Anirudh Konidala <kidskodingclub@gmail.com>
2026-01-22 02:21:21 -0600

    configured nvim with zathura and vimtex!

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,28 +1,31 @@
 return {
-    'lervag/vimtex',
-    lazy = false,
-      config = function()
-          vim.g.vimtex_view_method = 'zathura'
-          vim.g.vimtex_compiler_method = 'latexmk'
+  'lervag/vimtex',
+  lazy = false,
+  config = function()
+    vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_view_general_executable = 'zathura'
+    vim.g.vimtex_view_general_options = [[@pdf]]
+    vim.g.vimtex_view_automatic = 0
+    vim.g.vimtex_view_forward_search_on_start = 0
 
-          vim.g.tex_conceal = 'abdmg'
-          vim.g.vimtex_syntax_enabled = 1
+    vim.g.vimtex_compiler_latexmk = {
+      out_dir = 'build',
+      continuous = 1,
+    }
 
-          vim.g.vimtex_compiler_latexmk = {
-              build_dir = 'build',
-              options = {
-                  '-pdf',
-                  '-shell-escape',
-                  '-interaction=nonstopmode',
-                  '-synctex=1','-outdir=.'
-              },
-          }
-
-      vim.api.nvim_create_autocmd("BufWritePost", {
-        pattern = "*.tex",
-        command = "VimtexCompile"
-      })
-
+    local augroup = vim.api.nvim_create_augroup("VimtexLogic", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = augroup,
+      pattern = "tex",
+      callback = function()
+        if not vim.g.vimtex_started then
+          vim.g.vimtex_started = true
+          vim.cmd("VimtexCompile")
+          vim.defer_fn(function()
+            vim.cmd("VimtexView")
+          end, 1000)
+        end
       end,
+    })
+  end,
 }
-
